b2a211f72db2f7603695842655179aab6387db10
tylerjmwhit <tylerjmwhit@gmail.com>
2024-01-17 18:12:25 -0800

    finihsed part 2 of day_1: involded changing the regex to incorporate string varations of numbers. this meant I added a map to map the strings back to ints. Also involved changing the regex to work for overlapping patterens

--- Day_1/Day_1.py
+++ Day_1/Day_1.py
@@ -3,9 +3,24 @@ import re
 if __name__ == "__main__":
     # print("Hello world")
     sum = 0
+    word2num = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    'zero': '0'
+    }
     with open(r'Day_1\input.txt', "r") as file:
         for line in file:
-            x = re.findall("[0-9]",line)
-            value = (int(x[0]) * 10) + (int(x[-1]))
+            line = line.lower()
+            regex = r'(?=(one|two|three|four|five|six|seven|eight|nine|\d))'
+            x = re.findall(pattern = regex,string = line, flags = re.IGNORECASE)
+            x = [word2num.get(match,match)for match in x]
+            value = int(x[0] + x[-1])
             sum += value
     print(sum)
